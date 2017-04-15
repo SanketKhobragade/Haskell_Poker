6095a06795734923155babf85147e77e81458880
sumitmamtani <sumitmamtani04@gmail.com>
2017-04-15 10:46:11 +0530

    changes in game file

--- Game.hs
+++ Game.hs
@@ -1,4 +1,3 @@
-
 import Poker 
 import Data.List (tails)
 import Data.List
@@ -8,7 +7,6 @@ import System.Random
 import Shuffle
 import Player
 import Winner
-import Best5
 import Cpu
 
 get_userid ::Int ->  IO Int
@@ -22,95 +20,59 @@ get_userid turn = do
 		(get_userid turn)
 		
 -- Game function ReturnValue Type
-game :: [Player] -> Int -> Int -> [Card]-> IO Int
-game xs trn round cd = do
-	--print(trn)
-	if status (xs!!trn) == Fold then return (-1)
-	else if status (xs!!trn) == Allin then return (-2)
-	else if player_type (xs!!trn) == Human then do
+game :: [Player] -> Int -> Int -> IO Int
+game xs turn round = do
+	print(turn)
+	if status (xs!!turn) == Fold then return (-1)
+	else if status (xs!!turn) == Allin then return (-2)
+	else do
 		--print(1)
-		id <- (get_userid trn)
-		display round trn xs cd
+		id <- (get_userid turn)
+		display round turn xs
 		--print(4)
-		let min_raise = 2*((round_bet (xs!!trn)) - (bet (xs!!trn)))
+		let min_raise = 2*((round_bet (xs!!turn)) - (bet (xs!!turn)))
 		let call = round_bet (maximumBy (compare `on` round_bet) xs)
 		--print(7)
 		--print (round_bet (xs!!turn))
-		act <- link_action (action_decide xs trn) call min_raise (round_bet (xs!!trn))
+		act <- link_action (action_decide xs turn) call min_raise (round_bet (xs!!turn))
 		--print(10)
 		return act
-	else do
-		return (cpu_decide xs trn round cd)
 	
 
-turn :: [Player] -> Int -> Int -> Int -> [Card] -> IO [Player]
-turn p i n round cd = do
-	--print("Turn beg")
-	print_head
-	print_list (p) 0 
+turn :: [Player] -> Int -> Int -> Int -> IO [Player]
+turn p i n round = do
+	print("Turn beg")
 	--let x = map (\x -> round_bet x) p
-	--print(round_over p)
+	print(round_over p)
 	if (n > 5 && (round_over p)) || length (filter(\x -> status x == Play) p) == 1 then return p
 	else do 
-		bet <- game p i round  cd
-		(turn (action p i bet) ((i+1) `mod` 5) (n+1) round) cd
+		bet <- game p i round
+		(turn (action p i bet) ((i+1) `mod` 5) (n+1) round)
 
-round_game :: [Player] -> Int -> [Card] -> IO [Player]	
-round_game p round cd = do
+round_game :: [Player] -> Int -> IO [Player]	
+round_game p round = do
 	if round > 3 then return p
 	else do
-		--print ("BeginRound")
-		update1 <- turn p 0 1 round cd
+		print ("BeginRound")
+		update1 <- turn p 0 1 round
 		let update2 = clear_roundbet update1
-		--print ("EndRound")
-		round_game update2 (round+1) cd
-
+		print ("EndRound")
+		round_game update2 (round+1)
+		
 start_game :: [Player]	-> IO [Player]
 start_game p = do 
-	let shuffleList = shuffle c []
-	--x <- getLine
-	--let l1 = (maximumBy (compare `on` length) ((group . sort ) (shuffleList)))
-	let p_dist = dist_card (p) shuffleList
-	let l1 = (map (\x -> cards x) p_dist)
-	let community = comm_cards (drop 10 shuffleList)
-	let l2 = l1 ++ [community]
-	let l3 = (foldl (\acc x -> acc ++ x) [] l2)
-	--print (l3 )
-	after_round <- round_game p_dist 0 community
+	let p_dist = dist_card (playerList 0) (shuffle c [])
+	let community = comm_cards (drop 10 (shuffle c []))
+	after_round <- round_game p_dist 0
 	let best5list = hands after_round community
 	--print best5list
 	let win = pot best5list 
 	let init = map (\x -> x{status = Play, round_bet = 0, bet = 0, cards = [], top5 = []}) win
-	print_winner
-	print_winning_player (winning_player p)
-	--print init
+	print init
 	(start_game init)
-
-print_head :: IO ()
-print_head = do
-	putStrLn ("Name\tStatus\tChips\tBet\tRound Bet")
-
-print_list :: [Player] -> Int -> IO ()
-print_list p i = if i<=4 then do
-			putStrLn ((show (name pl)) ++ "\t" ++ (show (status pl)) ++"\t" ++ (show (chips pl)) ++ "\t" ++ (show (bet pl)) ++ "\t" ++ (show (round_bet pl)))
-			print_list p (i+1)
-		else putStrLn ""
-		where pl = (p!!i)
-			
-
-print_winner :: IO ()
-print_winner = do
-		putStrLn "Congratulation!! Winners : "
-		putStrLn("Name\tChips")
-
-print_winning_player :: [Player] -> IO ()
-print_winning_player (x:xs) = do
-				if (length xs == 0) then print_player x
-				else print_winning_player xs
-
-print_player :: Player -> IO ()
-print_player p = do 
-		putStrLn ((show (name p)) ++ "\t" ++ (show (chips p)))
-		
-main = do
-	start_game (playerList 0 0)
+	
+main = do 
+	x <- start_game (playerList 0)
+	print(minimum_bidder players_list)
+	--print(round_over players_list)
+	
